e6097bd34d11bce34ef8fb83f60badfa3f441bde
Tom Russell <tom.russell@ouce.ox.ac.uk>
2025-07-25 15:50:41 +0100

    Manual chunks for major libraries

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default {
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vega: ["vega-embed"],
+          maplibregl: ["maplibre-gl"],
+        },
+      },
+    },
+  },
 } satisfies UserConfig;
